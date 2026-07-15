beafd425f58a067a176b028566d0117117e6df59
Aylie Chou <aylie_chou@twreporter.org>
2026-07-16 01:38:56 +0800

    fix(lexical-editor): can't add divider in infobox

--- packages/lexical-editor/src/react/plugins/DividerPlugin/index.tsx
+++ packages/lexical-editor/src/react/plugins/DividerPlugin/index.tsx
@@ -9,8 +9,70 @@ import {
   $insertNodes,
   $isRangeSelection,
   COMMAND_PRIORITY_EDITOR,
+  type LexicalNode,
 } from 'lexical'
 import { useEffect, type FC } from 'react'
+
+import { $isInfoboxNode, type InfoboxNode } from '../InfoboxPlugin/nodes'
+
+function $findAncestorInfoboxNode(node: LexicalNode): InfoboxNode | null {
+  let currentNode: LexicalNode | null = node
+
+  while (currentNode) {
+    if ($isInfoboxNode(currentNode)) {
+      return currentNode
+    }
+
+    currentNode = currentNode.getParent()
+  }
+
+  return null
+}
+
+function $findDirectInfoboxChild(
+  node: LexicalNode,
+  infoboxNode: InfoboxNode
+): LexicalNode | null {
+  let currentNode: LexicalNode | null = node
+  let childNode: LexicalNode | null = null
+
+  while (currentNode && currentNode !== infoboxNode) {
+    childNode = currentNode
+    currentNode = currentNode.getParent()
+  }
+
+  return currentNode === infoboxNode ? childNode : null
+}
+
+function $insertDividerNode(): boolean {
+  const selection = $getSelection()
+  if (!$isRangeSelection(selection)) {
+    return false
+  }
+
+  const dividerNode = $createHorizontalRuleNode()
+  const anchorNode = selection.anchor.getNode()
+  const infoboxNode = $findAncestorInfoboxNode(anchorNode)
+
+  if (!infoboxNode) {
+    $insertNodes([dividerNode])
+    return true
+  }
+
+  const childNode = $findDirectInfoboxChild(anchorNode, infoboxNode)
+
+  if (!selection.isCollapsed()) {
+    selection.removeText()
+  }
+
+  if (childNode) {
+    childNode.insertAfter(dividerNode)
+  } else {
+    infoboxNode.append(dividerNode)
+  }
+
+  return true
+}
 
 const DividerPlugin: FC = () => {
   const [editor] = useLexicalComposerContext()
@@ -24,15 +86,7 @@ const DividerPlugin: FC = () => {
 
     return editor.registerCommand(
       INSERT_HORIZONTAL_RULE_COMMAND,
-      () => {
-        const selection = $getSelection()
-        if (!$isRangeSelection(selection)) {
-          return false
-        }
-
-        $insertNodes([$createHorizontalRuleNode()])
-        return true
-      },
+      () => $insertDividerNode(),
       COMMAND_PRIORITY_EDITOR
     )
   }, [editor])
